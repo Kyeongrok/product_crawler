ad54eb9afec71b40a796629427c8efdc8245015a
yeongseon <yeongseon.choe@gmail.com>
2017-09-15 00:09:05 +0900

    Update brackParse

--- src/parser/brackParse.js
+++ src/parser/brackParse.js
@@ -1,7 +1,7 @@
 const request = require('request');
 const cheerio = require('cheerio');
 
-const subParse = () => {
+const getTotalNumber = () => {
     console.log('subParser');
     const baseRequestOptions = {
         method: 'GET',
@@ -16,70 +16,105 @@ const subParse = () => {
                 reject(error);
             }
             // console.log(html);
-            console.log('success');
+            // console.log('success');
 
             // load website
             var $ = cheerio.load(html);
 
             // find total number of item
             $('.Count').each(function (index, elem) {
-                var total_item = Number($(this).text().split('Treffer')[0].replace(/\s/g, ''));
-                console.log(total_item);
-                resolve(total_item)
+                var totalNumber = Number($(this).text().split('Treffer')[0].replace(/\s/g, ''));
+                // console.log(totalNumber);
+                resolve(totalNumber)
             })
         });
     });
 }
 
-/*
-function paginationParse(index) {
-    requestOptions = paginationRequestOptions;
-    requestOptions["uri"] = requestOptions["uri"] + index
-    console.log(requestOptions)
-    request(request_options, function (error, response, html) {
-        if (error) {
-            throw error;
-        }
-        // console.log(html);
-
-        // load website
-        const $ = cheerio.load(html);
-        $('.productList__item').each(function (index, elem) {
-            //console.log($(this).text());
-            brand = $(this).children('div').children('a').children('span.productList__itemManufacturer').text();
-            name = $(this).children('div').children('a').children('span.productList__itemTitle').text();
-            // console.log(name);
-            price = $(this).children('div').children('a').children('div').children('div').children('em').text().replace(/\s/g, '');
-            console.log(brand);
-            console.log(name);
-            console.log(price);
-        })
-    })
-};
-*/
-
-const parse = () => {
-    console.log('brackParser');
-
-    return new Promise((resolve, reject) =>
-    {
-        let result = {list: []};
-        subParse()
-            .then((total_item) => {
-                console.log(total_item);
-                resolve(result);
-            })
-            .catch((error) => {
-                reject(error);
-            })
-    })
-
+const getList = () => {
+    console.log("getList");
     const paginationRequestOptions = {
         method: 'GET',
         uri: 'https://www.brack.ch/tv-audio-foto/tv-und-homecinema/tv/tv?page=',
         headers: {'User-Agent': 'Mozilla/5.0'},
     };
 
+    return new Promise((resolve, reject) => {
+
+        getTotalNumber()
+            .then((totalNumber) => {
+                let result = {list: []};
+
+                for (var index = 0; index < totalNumber / 20; index++) {
+                    var requestOptions = paginationRequestOptions;
+                    requestOptions["uri"] = requestOptions["uri"] + index
+                    request(requestOptions, function (error, response, html) {
+                        if (error) {
+                            // throw error;
+                            reject(error);
+                        }
+                        // console.log(html);
+                        console.log('success');
+
+                        // load website
+
+                        const $ = cheerio.load(html);
+                        $('.productList__item').each(function (index, elem) {
+                            //console.log($(this).text());
+                            brand = $(this).children('div').children('a').children('span.productList__itemManufacturer').text();
+                            name = $(this).children('div').children('a').children('span.productList__itemTitle').text();
+                            //console.log(name);
+                            price = $(this).children('div').children('a').children('div').children('div').children('em').text().replace(/\s/g, '');
+                            const productionInfo = {list: []};
+                            productionInfo.name = brand + " " + name;
+                            productionInfo.price = price;
+                            result.list.push(productionInfo);
+
+                            // console.log(brand);
+                            // console.log(name);
+                            // console.log(price);
+                        });
+                    })
+                }
+                resolve(result);
+            })
+            .catch((error) => {
+                reject(error);
+            })
+    });
+}
+
+const parse = () => {
+    console.log('brackParser');
+
+    return new Promise((resolve, reject) => {
+        //let results = {list: []};
+        getList()
+            .then((result) => {
+                resolve(result);
+            })
+            .catch((error) => {
+                reject(error);
+            })
+        /*
+        getTotalNumber()
+            .then((totalNumber) => {
+                console.log(totalNumber);
+
+                for (var i = 0; i < totalNumber / 20; i++) {
+                    getList((i+1), results.list)
+                        .then(result => results.push(result))
+                        .catch((error) => {
+                            reject(error);
+                        })
+                }
+                resolve(results);
+            })
+            .catch((error) => {
+                reject(error);
+            })
+            */
+    })
 }
 
 module.exports.parse = parse;
