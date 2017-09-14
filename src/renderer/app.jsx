35220ef3ea7fed115566e649e285e8d9027e0532
kyeongrok.kim <kyeongrokkim@okiconcession.com>
2017-09-14 11:45:58 +0900

    bootstrap 나오게 수정

--- src/renderer/app.jsx
+++ src/renderer/app.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './components/App';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
+import React from "react";
+import { render } from "react-dom";
+import App from "./components/App";
+//import 'bootstrap/dist/css/bootstrap.css';
+//import 'bootstrap/dist/css/bootstrap-theme.css';
 
-render(<App />, document.getElementById('app'));
+render(<App />, document.getElementById("app"));
